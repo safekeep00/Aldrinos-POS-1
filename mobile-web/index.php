64cdcea3050ec9d8fc1e6743466a04b1e234aed4
safekeep00@gmail.com <safekeep00@gmail.com>
2024-11-11 09:41:32 +0800

    Fix login page.

--- mobile-web/index.php
+++ mobile-web/index.php
@@ -1,3 +1,45 @@
+<?php
+    session_start();
+
+    $ipAddress = $_SERVER['REMOTE_ADDR'];
+
+    if (!isset($_SESSION["ip_address"])) {
+        $_SESSION["ip_address"] = $ipAddress;
+        $_SESSION["auth_retry"] = 0;
+        $_SESSION["auth_timestamp"] = date('h:i:s');
+    }
+
+    $latest_auth_timestamp = date('h:i:s');
+
+    $start = intval((explode(":", $_SESSION["auth_timestamp"]))[1]);
+    $end = intval((explode(":", $latest_auth_timestamp))[1]);
+    $mins = abs($end - $start);
+    $remaining = 5 - $mins;
+
+    if ($mins >= 5) {
+        $_SESSION["auth_retry"] = 0;
+        $_SESSION["auth_timestamp"] = date('h:i:s');
+    }
+
+     if ($_SESSION["auth_retry"] > 5) {
+        // echo "<pre>";
+        // print_r([
+        //     "auth_timestamp" => $_SESSION["auth_timestamp"],
+        //     "latest_auth_timestamp" => $latest_auth_timestamp,
+        //     "auth_retry" => $_SESSION["auth_retry"],
+        //     "end" => $end,
+        //     "start" => $start,
+        //     "mins" => $mins,
+        //     "remaining" => $remaining
+        // ]);
+        // echo "</pre>";
+        // echo "<br>";
+
+        echo "Max login attempt exceeded 5 times. Please try again after ${remaining} minute(s) and refresh the page. Click <a href='index.php'>here</a> to refresh.";
+        die();
+    }
+?>
+
 <html>
 
 <head>
@@ -49,18 +91,20 @@
 
 <body class="text-center">
     <main class="form-signin w-100 m-auto">
-        <form id="form-login" method="post" action="#">
+        <form id="form-login" method="post" action="auth.php">
             <h1 class="h3 mb-3 fw-normal text-white">Aldrinos</h1>
             <div class="form-floating">
-                <input type="text" class="form-control" id="employee-id" placeholder="Employee ID" required>
+                <input type="text" class="form-control" id="employee-id" placeholder="Employee ID" name="employee-id"
+                    required>
                 <label for="employee-id">Employee ID</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="employee-pass" placeholder="Password" required>
+                <input type="password" class="form-control" id="employee-pass" placeholder="Password"
+                    name="employee-pass" required>
                 <label for="employee-pass">Password</label>
             </div>
             <div id="err"></div>
-            <button id="btn-submit" class="w-100 btn btn-lg text-white rounded-pill" type="submit">Log In</button>
+            <button id="btn-submit" class="w-100 btn btn-lg text-white rounded-pill" name="submit" type="submit">Log In</button>
             <p class="mt-5 mb-3 text-white">&copy; 2024</p>
         </form>
     </main>
@@ -72,20 +116,18 @@
     <script src="js/history.js"></script>
     <script>
         $(document).ready(() => {
-            $("#form-login").submit((e) => {
-                e.preventDefault();
-                const employee_id = $("#employee-id").val();
-                const employee_pass = $("#employee-pass").val();
-                if (employee_id == "aldrinos" && employee_pass == "password123") {
-                    window.location.replace("dashboard.html");
-                } else {
-                    $("#err").html(`
-                        <div class="text-white mb-2">
-                            <span class="text-white">Invalid credentials.</span>
-                        </div>
-                    `);
-                }
-            });
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const error = urlParams.get('error');
+            const maxAttempt = "<?php echo $_SESSION["auth_retry"]; ?>";
+
+            if (error) {
+                $("#err").html(`
+                    <div class="text-white mb-2">
+                        <span class="text-white">Invalid credentials. Attempt ${maxAttempt}/5</span>
+                    </div>
+                `);
+            }
         });
     </script>
 </body>
